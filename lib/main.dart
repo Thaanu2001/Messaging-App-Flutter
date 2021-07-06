71b90e69fac2b8180d81e715f0e1cc245ff72024
Thaanu Perera <thaanu@Thaanus-MacBook-Pro.local>
2021-07-06 19:46:16 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MessagingApp());
+}
+
+class MessagingApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Messaging App',
+      //home: ,
+    );
+  }
+}
